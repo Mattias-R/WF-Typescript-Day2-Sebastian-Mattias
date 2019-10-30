cd3c80affe6fa83d9c4afcf4a33aa9e754eafd72
Mattias-R <mattias.r@gmx.at>
2019-10-30 11:03:34 +0100

    do not use document write

--- Basic/script.js
+++ Basic/script.js
@@ -4,8 +4,21 @@ class Person {
 		this.age = age;
 		this.jobTitle = jobTitle;
 	}
-	hello(){ console.log(`Hello there, My name is ${this.name} and I am ${this.age} years old, and I am a ${this.jobTitle}`);
+	hello(){ return `Hello there, My name is ${this.name} and I am ${this.age} years old, and I am a ${this.jobTitle}`
 	}
 }
 
 let haans = new Person("haans","67","unemplyed");
+
+class worker extends Person {
+	constructor(name,age,jobTitle,salary,jobLocation){
+		super(name,age,jobTitle);
+		this.salary = salary;
+		this.jobLocation = jobLocation;
+	}
+	hello(){return`${super.hello()} and I get ${this.salary} every month, and I work in ${this.jobLocation}
+` }
+}
+
+let peter = new worker("peter","73","teacher","1400","CodeFactory");
+document.write(peter.hello());
